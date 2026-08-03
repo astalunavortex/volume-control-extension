2c5175affee0445fc006bc05897f5a902c15ea8c
astalunavortex <astalunavortex@gmail.com>
2026-08-03 15:26:09 +0300

    feat: add tab storage cleanup onStartup

--- background.js
+++ background.js
@@ -1,3 +1,19 @@
 browser.tabs.onRemoved.addListener(async (tabId) => {
-    await browser.storage.local.remove(`vc_tab_${tabId}`);
+	await browser.storage.local.remove(`vc_tab_${tabId}`);
 });
+
+browser.runtime.onStartup.addListener(cleanupDeadTabs);
+
+async function cleanupDeadTabs() {
+	const allStorage = await browser.storage.local.get();
+	const allKeys = Object.keys(allStorage).filter(k => k.startsWith('vc_tab_'))
+	const tabIds = allKeys.map(k => parseInt(k.replace('vc_tab_', '')))
+
+	const tabs = await browser.tabs.query({});
+	const aliveIds = new Set(tabs.map(t => t.id));
+
+	const deadKeys = allKeys.filter((_, i) => !aliveIds.has(tabIds[i]));
+	if (deadKeys.length > 0) {
+		await browser.storage.local.remove(deadKeys);
+	}
+}
