8cb58c1b4408d2fb4d8b53ca65bad83cd28b543e
astalunavortex <astalunavortex@gmail.com>
2026-08-07 17:42:37 +0300

    fix: add error handeling to clenupDeadTabs

--- background.js
+++ background.js
@@ -10,15 +10,19 @@ browser.runtime.onStartup.addListener(cleanupDeadTabs);
 browser.runtime.onInstalled.addListener(cleanupDeadTabs);
 
 async function cleanupDeadTabs() {
-	const allStorage = await browser.storage.local.get();
-	const allKeys = Object.keys(allStorage).filter(k => k.startsWith('vc_tab_'))
-	const tabIds = allKeys.map(k => parseInt(k.replace('vc_tab_', '')))
+	try {
+		const allStorage = await browser.storage.local.get();
+		const allKeys = Object.keys(allStorage).filter(k => k.startsWith('vc_tab_'))
+		const tabIds = allKeys.map(k => parseInt(k.replace('vc_tab_', '')))
 
-	const tabs = await browser.tabs.query({});
-	const aliveIds = new Set(tabs.map(t => t.id));
+		const tabs = await browser.tabs.query({});
+		const aliveIds = new Set(tabs.map(t => t.id));
 
-	const deadKeys = allKeys.filter((_, i) => !aliveIds.has(tabIds[i]));
-	if (deadKeys.length > 0) {
-		await browser.storage.local.remove(deadKeys);
+		const deadKeys = allKeys.filter((_, i) => !aliveIds.has(tabIds[i]));
+		if (deadKeys.length > 0) {
+			await browser.storage.local.remove(deadKeys);
+		}
+	} catch(err) {
+		console.error('VC: cleanup failed', err);
 	}
 }
